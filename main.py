77972685f725449b1ae659ca98f70bf3b1976f4e
Sofiya Rao <sofiya.rao@coxautoinc.com>
2022-11-10 14:17:49 +0530

    repo structure & aws client config

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from tests.TestAppGraphics import TestAppGraphics
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_graphics = TestAppGraphics()
+    test_graphics.test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
