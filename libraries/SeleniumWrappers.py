74784ed65bdfdd3d286eafdcead0c2e794fb99f9
Abhishek-Sharma <Abhishek.Sharma@coxautoinc.com>
2022-11-25 14:32:49 +0530

    Fixed logger issue in selenium wrapper

--- libraries/SeleniumWrappers.py
+++ libraries/SeleniumWrappers.py
@@ -1,8 +1,12 @@
 
 import allure
+import logging
 from allure_commons.types import AttachmentType
 
 from tests.base_test import BaseTest
+
+
+logger = logging.getLogger(__name__)
 
 
 class SeleniumWrappers(BaseTest):
@@ -16,7 +20,7 @@ class SeleniumWrappers(BaseTest):
 
     def clear_element_text(self, element_id):
         self.get_element(element_id).clear()
-        self.logger.info("Text field" + element_id + "is cleared ")
+        logger.info("Text field" + element_id + "is cleared ")
         return self
 
     def input_element_text(self, input_text, element_id, element_type="id", ):
@@ -50,18 +54,16 @@ class SeleniumWrappers(BaseTest):
         expected_text = str(expected_text)
 
         try:
-                assert actual_text == expected_text
-                self.logger.info("Validation PASSED: Actual: {} and Expected: {}".format(actual_text,
-                                                                                              expected_text))
-
+            assert actual_text == expected_text
+            logger.info("Validation PASSED: Actual: {} and Expected: {}".format(actual_text,
+                                                                                expected_text))
         except AssertionError:
-            
-                self.logger.error("Validation FAILED: Actual: {} and Expected: {}".format(actual_text,
-                                                                                              expected_text))
-                allure.attach(name='Screenshot {}',
-                              body=self.driver.get_screenshot_as_png(),
-                              attachment_type=AttachmentType.PNG)
-                raise AssertionError("Please see screenshot")
+            logger.error("Validation FAILED: Actual: {} and Expected: {}".format(actual_text,
+                                                                                 expected_text))
+            allure.attach(name='Screenshot {}',
+                          body=self.driver.get_screenshot_as_png(),
+                          attachment_type=AttachmentType.PNG)
+            raise AssertionError("Please see screenshot")
                 
         return actual_text == expected_text
 
@@ -82,35 +84,35 @@ class SeleniumWrappers(BaseTest):
         try:
             element = driver_function(element_id)[element_index]
         except IndexError:
-            self.logger.error("Cannot find element - Type: %s, Name: %s",
-                              element_type, element_id)
-            return None
+            logger.error("Cannot find element - Type: %s, Name: %s",
+                         element_type, element_id)
+            return
         return element
 
     def select_checkbox_byflag(self, isflag, element_id, element_type="id"):
         # Select checkbox based on the isflag - True (Select) /False (unselect)
         if isflag:
-                self.select_checkbox(element_id, element_type="id")  # check checkbox if it is not checked
+            self.select_checkbox(element_id, element_type="id")  # check checkbox if it is not checked
         else:
-                self.unselect_checkbox(element_id, element_type="id")  # uncheck checkbox
+            self.unselect_checkbox(element_id, element_type="id")  # uncheck checkbox
 
         return self
 
     def select_checkbox(self, element_id, element_type="id"):
-        self.logger.info("Selecting checkbox '%s'." % element_id)
+        logger.info("Selecting checkbox '%s'." % element_id)
         element = self.get_element(element_id=element_id, element_type=element_type)
         element.click()
 
     def unselect_checkbox(self, element_id, element_type="id"):
         # Removes selection of checkbox identified by `locator`.
 
-        self.logger.info("Un-selecting checkbox '%s'." % element_id)
+        logger.info("Un-selecting checkbox '%s'." % element_id)
         element = self.get_element(element_id=element_id, element_type=element_type)
         if element.is_selected():
             element.click()
         return self
 
     def select_radio_button(self, element_id, element_type="id"):
-        self.logger.info("Selecting checkbox '%s'." % element_id)
+        logger.info("Selecting checkbox '%s'." % element_id)
         element = self.get_element(element_id=element_id, element_type=element_type)
         element.click()
